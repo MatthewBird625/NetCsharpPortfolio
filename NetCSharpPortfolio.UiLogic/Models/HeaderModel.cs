a53526a129132a2f8d715cf521d04f9bcfb86693
Matthew Bird <98002021+MatthewBird625@users.noreply.github.com>
2024-03-11 22:12:13 +1100

    remove duplicate project files

--- NetCSharpPortfolio.UiLogic/Models/HeaderModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NetCSharpPortfolio.UiLogic.Models
-{
-    public class HeaderModel
-    {
-        public string Name { get; set; } = string.Empty;
-        public string Role { get; set; } = string.Empty;
-        public string Linkedin { get; set; } = string.Empty;
-    }
-}
